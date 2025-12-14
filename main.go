d361f4b0331506607af5babd5257b861882422b1
dorofeyev <fdorofeyev@olimpbetkz.dev>
2025-12-14 13:00:46 +0500

    main - добавил работу с аргументами и публичный метод

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
+var name string
+
+func PrintHello(name string) {
+	fmt.Println("Hello, " + name)
+}
+
+func main() {
+	flag.StringVar(&name, "name", "", "Your name")
+	flag.Parse()
+	if name == "" {
+		log.Println("Please provide your name with -name flag")
+		flag.Usage()
+		os.Exit(2)
+	}
+	fmt.Println("Hello, " + name)
 }
